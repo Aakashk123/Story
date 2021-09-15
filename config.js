9ce796be8083c66c93a27a4c9a50a2e495578376
Aakashk123 <83151262+Aakashk123@users.noreply.github.com>
2021-09-15 12:37:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+ apiKey: "AIzaSyCDjqW14cFabJpmuS1OLOz2zCe9j1F9lGU",
+  authDomain: "story-telling-app-a3605.firebaseapp.com",
+  databaseURL: "https://story-telling-app-a3605-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-a3605",
+  storageBucket: "story-telling-app-a3605.appspot.com",
+  messagingSenderId: "1473812704",
+  appId: "1:1473812704:web:d7360197ce164ccf359885"
+};
